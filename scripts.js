d12981c0c8a3c0fc220c99d7e7419e6b5f136f6b
Notsofastmatey <mrsimonlaycock@gmail.com>
2020-05-23 23:23:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
-/*$(document).ready(function(){
-  
-  $("#fname").change(function(){
-  alert("The text has been changed.");
-});
-});
-*/
-
 $(document).ready(function(){
-  $("input").click(function(){
-    $(this).hide();
-  });
+  $("#fname").change(function(){
+  alert("The first name was changed at " + new Date().getTime());
+});
+  
+    $("#lname").change(function(){
+  alert("The last name was changed at " + new Date().getTime());
+});
+  
+  
 });
