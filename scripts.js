7e17f7afe35428105eca5c590470742de3075294
Notsofastmatey <mrsimonlaycock@gmail.com>
2020-05-24 11:10:23 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     $("#lname").change(function(){
       var time2 = new Date().getTime();
   alert("The last name was changed at " + time2);
-      $("#diff:text").val(time2-time1);
+      $("#diff:text").val("hi");
 });
   
   
