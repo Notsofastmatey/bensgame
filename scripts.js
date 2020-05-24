fa0c8c2e70fb910afa6c25595d4d60c7aa59975f
Notsofastmatey <mrsimonlaycock@gmail.com>
2020-05-24 11:23:50 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
   
   $("#q2").hide();
   $("#diff").hide();
-  $("#q1").change(function(){
+  $("#yourName").change(function(){
     time1 = new Date().getTime();
-  alert("The first name was changed at " + time1);
+  //alert("The first name was changed at " + time1);
   $("#q2").show();
 });
   
     $("#q2").change(function(){
       time2 = new Date().getTime();
-  alert("The last name was changed at " + time2);
+  //alert("The last name was changed at " + time2);
       difference = (time2-time1)/1000;
       $("#diff").show();
       $("#diff:text").val(difference);
